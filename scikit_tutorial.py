298533c5586f7ef0e4966f027acefaeea7ea5dec
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2020-08-05 01:17:44 -0300

    Dataset

---
+++ scikit_tutorial.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+
+# load dataset
+dataframe = pd.read_csv("sonar.csv", header=None)
+dataset = dataframe.values
+# split into input (X) and output (Y) variables
+X = dataset[:,0:60].astype(float)
+Y = dataset[:,60]
+
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.fit_transform(Y)
+
+print(encoded_Y)
